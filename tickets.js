f1cd075534b02d83c57c6f520b3e341931755f34
RhysSerjent <75740518+RhysSerjent@users.noreply.github.com>
2021-05-15 15:02:15 +0100

    upload files

---
+++ tickets.js
@@ -0,0 +1,161 @@
+let cost = 0;
+let adult = 0;
+let oap = 0;
+let child = 6;
+let family = 0;
+
+const adultReturn = 4;
+const concessionReturn = 3;
+const familyReturn = 10;
+
+const adultSingle = 3;
+const concessionSingle = 2;
+const familySingle = 6;
+
+function resetValues() {
+    adult = document.getElementById('aT').value;
+    oap = document.getElementById('oT').value;
+    child = document.getElementById('cT').value;
+    family = 0;
+
+    adultReturnTickets = 4;
+    concessionReturnTickets = 3;
+    familyReturnTickets = 10;
+}
+
+function inputChanged() {
+
+    let ticket = null;
+
+    resetValues();
+    aacc();
+    aocc();
+    accc();
+    oocc();
+    occc();
+    cccc();
+    aac();
+    acc();
+    aoc();
+
+    const outPutA = document.getElementById('outputAdult');
+    const outPutC = document.getElementById('outputConcession');
+    const outPutF = document.getElementById('outputFamily');
+    const outPutCost = document.getElementById('outputCost')
+
+
+    if (document.getElementById('rdbSingle').checked === true) {
+        ticket = 'Single';
+        cost = (family * familySingle) + (adult * adultSingle) + ((Number(child) + Number(oap)) * concessionSingle)
+    }else{
+        ticket = 'Return';
+        cost = (family * familyReturn) + (adult * adultReturn) + ((Number(child) +Number(oap)) * concessionReturn)
+
+    }
+    
+    outPutF.textContent = `Family ${ticket} X ${family}`;
+    outPutA.textContent = `Adult ${ticket} X ${adult} `;
+    outPutC.textContent = `Concession ${ticket} X ${Number(child) + Number(oap)}`
+    outPutCost.textContent = `Total cost £${cost}`;
+
+}
+
+function aacc() {
+    while (adult >= 2  && child >= 2) {
+            adult -= 2;
+            child -= 2;
+            family += 1;
+
+    }
+}
+
+
+function aocc() {
+    while (adult >= 1 && oap >= 1 && child >= 2) {
+        adult -=1;
+        oap -= 1;
+        child -=2
+        family += 1;
+    }
+}
+
+
+function accc() {
+    while (adult >= 1 && child >= 3) {
+        adult -= 1;
+        child -=3;
+        family += 1
+    }
+}
+
+function oocc() {
+    while (oap >= 2 && child >= 2) {
+        oap -= 2;
+        child -= 2;
+        family +=1;
+    }
+}
+
+function occc() {
+    while (oap >= 1 && child >= 3) {
+        oap -= 1;
+        child -= 3;
+        family += 1;
+    }
+ 
+}
+
+function cccc() {
+    while (child >= 4) {
+        child -=4;
+        family += 1;
+    }
+}
+
+function aac() {
+    while (adult >= 2 && child >= 1) {
+        adult -= 2;
+        child -= 1;
+        family +=1;
+    }
+}
+
+function acc(){
+    while (adult >=1 && child >= 2) {
+        adult -=1;
+        child -= 2;
+        family += 1;
+
+    } 
+}
+
+function aoc() {
+    while (adult >= 1 && child >= 1 && oap >= 1) {
+        adult -=1;
+        child -= 1;
+        oap -= 1;
+        family += 1;
+    }
+}
+
+function clear() {
+    document.getElementById('aT').value = 0;
+    document.getElementById('oT').value = 0;
+    document.getElementById('cT').value = 0;
+
+    const pOut = document.getElementsByTagName('p');
+    for (let i = 0; i < pOut.length; i++) {
+        pOut[i].textContent = '';
+    }
+
+}
+
+
+let inputs = document.getElementsByTagName('input');
+for (let i = 0; i < inputs.length; i++) {
+    let iBox = inputs[i];
+    iBox.addEventListener('input',inputChanged);
+    
+}
+
+document.getElementById('clear').addEventListener('click', clear);
