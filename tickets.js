158c30ef2d0d88912fea3c1f9df918ccd0502f72
UP2015742 <91884099+UP2015742@users.noreply.github.com>
2022-03-22 23:00:45 +0000

    code clean up

--- tickets.js
+++ tickets.js
@@ -4,6 +4,7 @@ let oap = 0;
 let child = 0;
 let family = 0;
 
+// ticket prices
 const adultReturn = 5;
 const concessionReturn = 3;
 const familyReturn = 12;
@@ -13,117 +14,86 @@ const adultSingle = 3;
 const concessionSingle = 2;
 
 
-function vaildCheck(handler){
-    if (handler.value < 0 || handler.value == '') {
-        handler.value = null;
-        return 0;
-        
-        
-        
-    }else
-        return handler.value;
-
+function returnValidInput(handler) {
+  if (handler.value === '') {
+    handler.value = null;
+    return 0;
+  } else if (handler.value < 0) {
+    handler.value *= -1;
+  }
+  return handler.value;
 }
 
-function resetValues() {
-
-
-    adult = vaildCheck(document.getElementById('aT'));
-    oap = vaildCheck(document.getElementById('oT'));
-    child = vaildCheck( document.getElementById('cT'));
-    family = 0;
-
-
-    if (adult < 0 ) {
-        adult *= -1
-        document.getElementById('aT').value = adult
-    }
-
-    if (adult == '') {
-        adult = 0;
-    }
+function updateValues() {
+  adult = returnValidInput(document.getElementById('aT'));
+  oap = returnValidInput(document.getElementById('oT'));
+  child = returnValidInput(document.getElementById('cT'));
+  family = 0;
 }
 
 function inputChanged() {
+  let ticket = null;
 
-    let ticket = null;
+  updateValues();
 
-    resetValues();
-
-
-
-    if (document.getElementById('rdbSingle').checked === true) {
-        ticket = 'Single';
-        cost = (family * familyReturn) + (adult * adultSingle) + ((Number(child) + Number(oap)) * concessionSingle)
-
-
-    }else{
-        ticket = 'Return';
-        familyTicketBuilder(2,2,0);
-        familyTicketBuilder(1,2,1);
-        familyTicketBuilder(0,2,2);
-        familyTicketBuilder(2,1,0);
+  if (document.getElementById('rdbSingle').checked === true) {
+    ticket = 'Single';
+    cost = (family * familyReturn) + (adult * adultSingle) + ((Number(child) + Number(oap)) * concessionSingle);
+  } else {
+    ticket = 'Return';
+    // groups that are cheaper as a family ticket 
+    familyTicketBuilder(2, 2, 0);
+    familyTicketBuilder(1, 2, 1);
+    familyTicketBuilder(0, 2, 2);
+    familyTicketBuilder(2, 1, 0);
 
 
-        cost = (family * familyReturn) + (adult * adultReturn) + (oap * seniorReturn) + (Number(child) * concessionReturn);
-    }
+    cost = (family * familyReturn) + (adult * adultReturn) + (oap * seniorReturn) + (Number(child) * concessionReturn);
+  }
 
-    const outPutA = document.getElementById('outputAdult');
-    const outPutC = document.getElementById('outputConcession');
-    const outPutS = document.getElementById('outputSenior')
-    const outPutF = document.getElementById('outputFamily');
-    const outPutCost = document.getElementById('outputCost')
+  const outPutA = document.getElementById('outputAdult');
+  const outPutC = document.getElementById('outputConcession');
+  const outPutS = document.getElementById('outputSenior');
+  const outPutF = document.getElementById('outputFamily');
+  const outPutCost = document.getElementById('outputCost');
 
-    outPutS.textContent = '';
+  outPutS.textContent = '';
 
-    
-    outPutF.textContent = `Family Return X ${family}`;
-    outPutA.textContent = `Adult ${ticket} X ${adult} `;
-    if (document.getElementById('rdbSingle').checked === true){
-        outPutC.textContent = `Concession ${ticket} X ${Number(child) + Number(oap)}`
-    }else {
-        outPutC.textContent = `Concession ${ticket} X ${Number(child)}`
-        outPutS.textContent = `Senior ${ticket} X ${Number(oap)}`
-    }
-
-
-
-    // outPutC.textContent = `Concession ${ticket} X ${Number(child) + Number(oap)}`
-    outPutCost.textContent = `Total cost £${cost}`;
-
+  outPutF.textContent = `Family Return X ${family}`;
+  outPutA.textContent = `Adult ${ticket} X ${adult} `;
+  if (document.getElementById('rdbSingle').checked === true) {
+    outPutC.textContent = `Concession ${ticket} X ${Number(child) + Number(oap)}`;
+  } else {
+    outPutC.textContent = `Concession ${ticket} X ${Number(child)}`;
+    outPutS.textContent = `Senior ${ticket} X ${Number(oap)}`;
+  }
+  outPutCost.textContent = `Total cost £${cost}`;
 }
 
-
-
-
-function familyTicketBuilder(a,c,o) {
-    while (adult >= a && child >= c && oap >= o) {
-        adult -= a;
-        child -= c;
-        oap -= o;
-        family += 1;
-
-    }
+function familyTicketBuilder(a, c, o) {
+  while (adult >= a && child >= c && oap >= o) {
+    adult -= a;
+    child -= c;
+    oap -= o;
+    family += 1;
+  }
 }
 
 function clear() {
-    document.getElementById('aT').value = null;
-    document.getElementById('oT').value = null;
-    document.getElementById('cT').value = null;
+  document.getElementById('aT').value = null;
+  document.getElementById('oT').value = null;
+  document.getElementById('cT').value = null;
 
-    const pOut = document.getElementsByTagName('p');
-    for (let i = 0; i < pOut.length; i++) {
-        pOut[i].textContent = '';
-    }
-
+  const pOut = document.getElementsByTagName('p');
+  for (let i = 0; i < pOut.length; i++) {
+    pOut[i].textContent = '';
+  }
 }
 
-
-let inputs = document.getElementsByTagName('input');
+const inputs = document.getElementsByTagName('input');
 for (let i = 0; i < inputs.length; i++) {
-    let iBox = inputs[i];
-    iBox.addEventListener('input',inputChanged);
-    
+  const iBox = inputs[i];
+  iBox.addEventListener('input', inputChanged);
 }
 
 document.getElementById('clear').addEventListener('click', clear);
